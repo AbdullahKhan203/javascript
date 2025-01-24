6c712cbfab4fc667963b8ec79d9d6b6b0c88c7e2
AbdullahKhan203 <hk7485966@gmail.com>
2025-01-25 00:19:42 +0500

    dom lect 54 done

---
+++ 54-dom/started-code/script.js
@@ -0,0 +1,12 @@
+function sayHi(){
+   document.body.children[3].src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUPIfiGgUML8G3ZqsNLHfaCnZK3I5g4tJabQ&s"
+}
+
+// sayHi()
+let myPare=document.children[0].children[1].children[5]
+console.log(myPare)
+myPare.innerText="Change Text"
+
+
+console.log(document.body.children[0])
+document.body.children[0].innerText="backend development"
