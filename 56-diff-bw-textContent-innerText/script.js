89d4b5bf274d2fd40792543687247be0d76cd309
AbdullahKhan203 <hk7485966@gmail.com>
2025-01-28 17:13:38 +0500

    doned 56,57 lecture

---
+++ 56-diff-bw-textContent-innerText/script.js
@@ -0,0 +1,7 @@
+let para=document.querySelector('p')
+
+// para.innerHTML="<b>Hii</b>"
+// console.log(para)
+// console.log(para.innerHTML)
+console.log(para.innerText)
+// console.log(para.textContent)
---
+++ 57-getAttribute-setAttribute/script.js
@@ -0,0 +1,9 @@
+let para=document.querySelector('p')
+
+// para.innerHTML="<b>Hii</b>"
+// console.log(para)
+// console.log(para.innerHTML)
+console.log(para.innerText)
+// console.log(para.textContent)
+
+console.log(document.querySelector("h1").setAttribute("hello","ali"))
